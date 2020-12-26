5081177549365044294e2d67cb2efc4aee1ac2a0
= <qwert@gmail.com>
2020-12-26 16:58:19 +0500

    js1

---
+++ script.js
@@ -0,0 +1,20 @@
+var str = prompt('Введите свое имя')
+console.info( 'Меня зовут '+ str)
+
+ var num = +prompt('Введите свой возраст')
+ console.info('Возраст ' + num)
+
+var first = +prompt('6 + 3')
+console.info('Решение: '+'6 + 3 = ' + (6 + 3) + '  Ваш ответ: ' + first)
+
+var second = +prompt('15 - 9')
+console.info('Решение: '+'15 - 9 = ' + (15 - 9) + '  Ваш ответ: ' + second)
+
+var third = +prompt('20 * 3')
+console.info('Решение: '+'20 * 3 = ' + 20 * 3 + '  Ваш ответ: ' + third)
+
+var fourth = +prompt('81 / 3')
+console.info('Решение: '+'81 / 3 = ' + 81 / 3 + '  Ваш ответ: ' + fourth)
+
+var fifth = +prompt('72 % 5')
+console.info('Решение: '+'72 % 5 = ' + 72 % 5 + '  Ваш ответ: ' + fifth)
